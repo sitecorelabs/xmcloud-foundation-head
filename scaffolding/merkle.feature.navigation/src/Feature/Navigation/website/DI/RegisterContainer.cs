5260594f3a84f82c0005d34c22ea4a36a450a0df
Chet Potvin <CPotvin@degdigital.com>
2023-04-26 11:50:37 -0500

    rough Navigation feature

---
+++ scaffolding/merkle.feature.navigation/src/Feature/Navigation/website/DI/RegisterContainer.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Sitecore.DependencyInjection;
+
+namespace merklestarter.Feature.Navigation.DI
+{
+    public class RegisterContainer : IServicesConfigurator
+    {
+        public void Configure(IServiceCollection serviceCollection)
+        {
+
+        }
+    }
+}
