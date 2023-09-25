866d3ce4fdb108c18c9b8a1d3a3d0cd3c4bb3691
SitecoreAdmin <SitecoreAdmin@RSMDocker-VM>
2023-09-25 07:23:51 +0000

    Fix build error

--- src/sxastarter/src/pages/_app.tsx
+++ src/sxastarter/src/pages/_app.tsx
@@ -3,7 +3,7 @@ import { I18nProvider } from 'next-localization';
 import { SitecorePageProps } from 'lib/page-props';
 
 import 'assets/main.scss';
-import 'assets/EspireComponent/DXSydney.css';
+
 function App({ Component, pageProps }: AppProps<SitecorePageProps>): JSX.Element {
   const { dictionary, ...rest } = pageProps;
 
