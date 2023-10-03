dfdb62c6c1ebe972557c05c26315409eed1fb7ad
SitecoreAdmin <SitecoreAdmin@RSMDocker-VM>
2023-10-03 12:05:00 +0000

    Undo PlainHTMLResuable Component Changes

--- src/sxastarter/src/components/Espire/PageContent/PlainHTMLResuable.tsx
+++ src/sxastarter/src/components/Espire/PageContent/PlainHTMLResuable.tsx
@@ -1,20 +1,12 @@
 import React from 'react';
 import { PlainHTMLResuableTemplateProps } from 'lib/component-props/EspireTemplateProps/PageContent/PlainHTMLResuableTemplateProps';
-import { withDatasourceCheck, Text } from '@sitecore-jss/sitecore-jss-nextjs';
-import { EditMode } from 'lib/component-props';
+import { withDatasourceCheck } from '@sitecore-jss/sitecore-jss-nextjs';
 
 export const PlainHTMLResuable = (props: PlainHTMLResuableTemplateProps): JSX.Element => {
-  const editMode = EditMode();
   return (
-    <>
-      {editMode ? (
-        <Text field={props?.fields?.PlainHTML} />
-      ) : (
-        <div className={`plain-text-reusable ${props.params.styles}`}>
-          <div dangerouslySetInnerHTML={{ __html: props?.fields?.PlainHTML?.value }} />
-        </div>
-      )}
-    </>
+    <div className={`plain-text-reusable ${props.params.styles}`}>
+      <div dangerouslySetInnerHTML={{ __html: props?.fields?.PlainHTML?.value }} />
+    </div>
   );
 };
 
