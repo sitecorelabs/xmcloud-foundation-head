34636254dd99b7599f266bed36a8caa44eda4a5c
gurpreet.singh <gurpreet.singh@espire.com>
2023-11-03 14:54:53 +0530

    language selector -  with limited numbers

--- src/sxastarter/src/components/Espire/Header/LanguageSelector.tsx
+++ src/sxastarter/src/components/Espire/Header/LanguageSelector.tsx
@@ -1,3 +1,4 @@
+import GoogleTranslate from 'components/GoogleTranslate';
 import { ComponentProps } from 'lib/component-props';
 import Form from 'react-bootstrap/Form';
 
@@ -5,12 +6,13 @@ const LanguageSelector = (props: ComponentProps): JSX.Element => {
   return (
     <div className={`${props.params.styles}`}>
       <div className="language-selector">
-        <Form.Select aria-label="Default select example">
+        <GoogleTranslate />
+        {/* <Form.Select aria-label="Default select example">
           <option>Select Language</option>
           <option value="1">English</option>
           <option value="2">French</option>
           <option value="3">German</option>
-        </Form.Select>
+        </Form.Select> */}
       </div>
     </div>
   );
